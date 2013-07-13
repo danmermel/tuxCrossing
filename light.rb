289349fffe40c01ff516bc4920c3bb02e24b90db
Daniel Mermelstein <danmermel@gmail.com>
2013-07-13 11:44:38 +0000

    initial commit

---
+++ light.rb
@@ -0,0 +1,27 @@
+require 'pi_piper'
+include PiPiper
+
+green_pin = PiPiper::Pin.new( :pin => 17, :direction => :out)
+red_pin = PiPiper::Pin.new( :pin => 27, :direction => :out)
+# button = PiPiper::Pin.new( :pin => 2, :direction => :in)
+
+
+after :pin => 2, :goes => :low do
+   puts "button pressed"
+   puts "now starting sequence"
+   puts "green pin going on"
+   green_pin.on
+   sleep 3
+   puts "red pin going on"
+   red_pin.on
+   sleep 3
+   puts "green pin going off"
+   green_pin.off
+   puts green_pin.read
+   sleep 3
+   puts "red pin going off"
+   red_pin.off
+end
+
+PiPiper.wait
+
