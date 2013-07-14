50f66be600a540da4a3d0eaa20c3964d3debff09
Daniel Mermelstein <danmermel@gmail.com>
2013-07-14 09:34:18 +0000

    lighting sequence finished

--- light.rb
+++ light.rb
@@ -1,33 +1,47 @@
 require 'pi_piper'
 include PiPiper
 
-green_pin = PiPiper::Pin.new( :pin => 17, :direction => :out)
-red_pin = PiPiper::Pin.new( :pin => 27, :direction => :out)
-# button = PiPiper::Pin.new( :pin => 2, :direction => :in)
+$green_pin = PiPiper::Pin.new( :pin => 17, :direction => :out)
+$red_pin = PiPiper::Pin.new( :pin => 27, :direction => :out)
+$button = PiPiper::Pin.new( :pin =>2, :direction => :in)
+$amber_pin = PiPiper::Pin.new( :pin => 10, :direction => :out)
+$ped_green = PiPiper::Pin.new( :pin => 15, :direction => :out)
+$ped_red = PiPiper::Pin.new( :pin => 18, :direction => :out)
 
-watch :pin=> 2 do
-   puts "Pin changed from #{last_value} to #{value}"
+after( :pin => 2, :goes => :low) do  # start crossing sequence
+  $green_pin.off
+  $amber_pin.on
+  sleep 2
+  $amber_pin.off
+  $red_pin.on
+  sleep 0.5
+  $ped_red.off
+  $ped_green.on
+  sleep 5
+  $red_pin.off
+  
+  5.times do    #flashing lights
+    $amber_pin.on
+    $ped_green.on
+    sleep 0.5
+    $amber_pin.off
+    $ped_green.off
+    sleep 0.5
+  end
+  $ped_red.on
+  $green_pin.on
+  
+     
 end
 
-=begin 
-after( :pin => 2, :goes => :low) do
-   puts "button pressed"
-   puts "now starting sequence"
-   puts "green pin going on"
-   green_pin.on
-   sleep 3
-   puts "red pin going on"
-   red_pin.on
-   sleep 3
-   puts "green pin going off"
-   green_pin.off
-   puts green_pin.read
-   sleep 3
-   puts "red pin going off"
-   red_pin.off
+def starting_position   # make sure all the leds are in the right position
+  $ped_red.on
+  $green_pin.on
+  $amber_pin.off
+  $red_pin.off
+  $ped_green.off
 end
 
-=end
-
+starting_position
 PiPiper.wait
 
